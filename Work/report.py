1911a4d3e0b5f3a1026c3bb8061455c75efce325
arjun <nagarjunv1990@gmail.com>
2021-06-08 08:04:27 +0530

    inventory_dict

--- Work/report.py
+++ Work/report.py
@@ -9,7 +9,10 @@ def read_inventory(filename):
         inventory = list()
 
         for row in rows:
-            product = row[0], int(row[1]), float(row[2])
+            product = dict()
+            product["name"] = row[0]
+            product["quant"] = int(row[1])
+            product["price"] = float(row[2])
             inventory.append(product)
     return inventory
 
