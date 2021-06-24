80ec6f9149f90e2496f5eb44b1261f2496795bc7
nvelmuru <nvelmurugan@linkedin.com>
2021-06-24 08:18:46 +0530

    Reading files without headers

--- Work/file_parse.py
+++ Work/file_parse.py
@@ -1,35 +1,49 @@
 import csv
 
 
-def parse_csv(filename, select = None , types = None):
+def parse_csv(filename, select = None , types = None, has_headers = True):
     """
     Parse a csv fike into a list of records
     :param filename: filename
     :param select : specify  List of column name
     :param type : Datatype of each column
+    :param has_headers : True for Files with Headers
     :return: list if dict
     """
     with open(filename) as FH:
         rows = csv.reader(FH)
-        headers = next(rows)
-        records = list()
+        # Read the file headers if presents
+        if has_headers:
+            headers = next(rows)
+        else:
+            headers = []
+
+        # If specific column have been selected , make indices for filtering
         if select:
             indices = [headers.index(column) for column in select]
             headers = select
         else:
             indices = []
 
+        records = list()
         for row in rows:
             if not row:
                 continue
 
+            # If specific column indices are selected , pick them out
             if indices:
                 row = [row[index] for index in indices]
 
+            # apply type conversion to the row
             if types:
                 row = [func(val) for func, val in zip(types, row)]
 
-            record = dict(zip(headers, row))
+            # make dict on a tuple
+            if headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+
             records.append(record)
 
     return records
