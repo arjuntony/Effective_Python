3c544d36496847b16af17b592941b6bf015dbd3e
nvelmuru <nvelmurugan@linkedin.com>
2021-07-22 09:32:19 +0530

    product.py with class objects

---
+++ Work/product.py
@@ -0,0 +1,9 @@
+class Product:
+
+    def __init__(self, name, quant, price):
+        self.name = name
+        self.quant = quant
+        self.price = price
+    # no return statement in init
+
+
