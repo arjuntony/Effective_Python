fafd44ae5607ce0fafe916af275b37426e0f3247
nvelmuru <nvelmurugan@linkedin.com>
2021-08-11 23:02:56 +0530

    added containers

--- Work/inventory.py
+++ Work/inventory.py
@@ -5,6 +5,15 @@ class Inventory:
     def __iter__(self):
         return self._pdcts.__iter__()
 
+    def __len__(self):
+        return len(self._pdcts)
+
+    def __getitem__(self, index):
+        return self._pdcts[index]
+
+    def __contains__(self, name):
+        return any([p.name == name for p in self._pdcts])
+
     @property
     def total_cost(self):
         return sum([p.cost for p in self._pdcts])
