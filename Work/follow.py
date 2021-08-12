c7d909e7204339d75a1c99402956eb2ffa95daea
nvelmuru <nvelmurugan@linkedin.com>
2021-08-12 22:57:17 +0530

    using generator to prodice data(Serverlogs)

--- Work/follow.py
+++ Work/follow.py
@@ -1,17 +1,28 @@
 import os
 import time
 
-f = open('Data/marketlog.csv')
-f.seek(0, os.SEEK_END)   # Move file pointer 0 bytes from end of file
 
-while True:
-    line = f.readline()
-    if line == '':
-        time.sleep(0.1)   # Sleep briefly and retry
-        continue
-    fields = line.split(',')
-    name = fields[0].strip('"')
-    price = float(fields[1])
-    change = float(fields[4])
-    if change < 0:
-        print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+def follow(filename):
+    f = open(filename)
+    f.seek(0, os.SEEK_END)  # Move file pointer 0 bytes from end of file
+    while True:
+        line = f.readline()
+        if line == '':
+            time.sleep(0.1)  # Sleep briefly and retry
+            continue
+
+        yield line
+
+
+if __name__ == '__main__':
+    import report
+    inventory = report.read_inventory("Data/inventory.csv")
+
+    for line in follow("Data/marketlog.csv"):
+        fields = line.split(',')
+        name = fields[0].strip('"')
+        price = float(fields[1])
+        change = float(fields[4])
+        if name in inventory:
+            print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+843509
